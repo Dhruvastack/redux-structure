cf3b2bc4f0aea6e0c1e0503fca0fd8c90d625346
Dhruva <dhruvas.stacksum@gmail.com>
2021-03-24 18:47:54 +0530

    file structure redux

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import {createStore} from 'redux'
+import rootReducer from './reducers'
+import {Provider} from 'react-redux'
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
