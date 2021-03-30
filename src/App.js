c02bb1e4107379b4933d2348e4cf63dd80abc768
Dhruva <dhruvas.stacksum@gmail.com>
2021-03-30 18:49:59 +0530

    added the route files too

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Users } from "./containers/Users";
 import { Login } from "./containers/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Users />
-      <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Users} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
